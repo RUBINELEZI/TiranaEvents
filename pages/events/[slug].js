bd37bdf151ca7a5233fd7930dee52b5765032cdf
Rubin <rubinelezi0@gmail.com>
2021-06-05 18:01:08 +0200

    SingleEvent page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -3,32 +3,13 @@ import Layout from "../../components/Layout";
 import { useState } from "react";
 import { API_URL } from "../../config";
 import EventItem from "../../components/EventItem";
+import SingleEvent from "../../components/SingleEvent";
 
 export default function EventPage({ res }) {
-  const router = useRouter();
-  const [event, setEvent] = useState({});
-  // useEffect(() => {
-  //     axios.get(`${API_URL}/api/events/${router.query.slug}`)
-  //         .then(function (response) {
-  //             // handle success
-  //             setEvent(response.data[0])
-  //         })
-  //         .catch(function (error) {
-  //             // handle error
-  //             console.log(error);
-  //         })
-  // }, [router.query.slug])
-
   return (
     <div>
       <Layout>
-        <EventItem
-          vendor={res[0].vendor}
-          performer={res[0].performer}
-          date={res[0].date}
-          time={res[0].time}
-          info={res[0].info}
-        />
+        <SingleEvent event={res[0]} />
       </Layout>
     </div>
   );
