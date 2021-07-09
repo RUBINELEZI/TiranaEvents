6c1aee0ed0c9dd7d61dd60305019c522937f30cd
rubinelezi <rubin.elezi@magoware.tv>
2021-07-09 17:43:54 +0200

    createt add form structure

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,3 +1,47 @@
+import Layout from "../../components/Layout";
+import { API_URL } from "../../config/index";
+import styles from "../../styles/AddPage.module.css";
+import Link from "next/link";
+
 export default function add() {
-  return <div></div>;
+  return (
+    <Layout>
+      <Link href={"/"}>GO BACK</Link>
+      <h1>Add Event</h1>
+      <div className={styles.container}>
+        <div>
+          <label htmlFor="">TEST</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">TEST</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">TEST</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">TEST</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">TEST</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">TEST</label>
+          <input type="text" />
+        </div>
+        <div className={styles.descript}>
+          <label htmlFor="">TEST</label>
+          <textarea type="text"></textarea>
+        </div>
+        <div className={styles.btn}>
+          <label htmlFor="">TEST</label>
+          <button className={"btn"} type="text"> Add Event</button>
+        </div>
+      </div>
+    </Layout>
+  );
 }
